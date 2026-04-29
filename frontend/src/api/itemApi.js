6e3297b0dbbb9c2cddf2e684ff7d724ea3b91230
Mithila_Dissanayaka <mithiladissanayaka1422@gmail.com>
2026-04-29 16:10:34 +0530

    update backend URL

--- frontend/src/api/itemApi.js
+++ frontend/src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://wmt-lab-test-production-40de.up.railway.app/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://wmt-lab-test-production-40de.up.railway.app/api",
 });
 
 export const getItems = () => API.get("/items");
